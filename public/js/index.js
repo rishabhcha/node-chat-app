1e2a4a5ca9da36232dcbfc7dcac8d3d686f5e1f2
rishabhcha <rishabhchandel1996@gmail.com>
2017-09-20 22:48:36 +0530

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function(){
     console.log('connected to server');
-
-    socket.emit('createMessage', {
-        from: 'rishabh',
-        text: 'yep, working'
-    });
 });
 
 socket.on('disconnect', function() {
